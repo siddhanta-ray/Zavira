1ac7a6503798399b5ad214e63c47be91c607d4f6
Tameem2004 <mdtameem.gazi2004@gmail.com>
2025-06-20 22:20:24 +0530

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import VisionMission from "./components/VisionMission";
@@ -9,7 +8,6 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="overflow-y-scroll snap-y snap-mandatory scroll-smooth scrollbar-hide h-screen">
-      <Header />
       <HeroSection />
       <AboutSection />
       <VisionMission />
