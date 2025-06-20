95b6a37d3d699418d6f9e10afbd6e5052fb26861
Tameem2004 <mdtameem.gazi2004@gmail.com>
2025-06-20 23:19:46 +0530

    auth

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { createCivicAuthPlugin } from "@civic/auth/nextjs"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withCivicAuth = createCivicAuthPlugin({
+  clientId: "34c577e8-dbfd-455b-bd47-f69f04d96190"
+});
+
+export default withCivicAuth(nextConfig)
