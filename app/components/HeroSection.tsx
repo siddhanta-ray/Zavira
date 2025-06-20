dc1b4401824ee8a7dbbd9a20b5c1146c3e19cfcc
Tameem2004 <mdtameem.gazi2004@gmail.com>
2025-06-21 00:27:54 +0530

    auth

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@civic/auth/react";
 export default function HeroSection() {
   return (
     <section className="h-screen snap-start bg-lime-200 pt-24 px-6 flex flex-col items-center">
       <div className="max-w-lg">
+              <UserButton />
         <h1 className="text-3xl font-bold mb-4">Lorem ipsum dolor sit amet</h1>
         <p className="mb-4">Consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
         <button className="bg-blue-600 text-white px-4 py-2 rounded">Get Started</button>
