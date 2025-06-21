2bc930d05e7d0b082e592189ddca26a9f1c6e6c7
Ayushi Mitra <ayushimitra22@gmail.com>
2025-06-21 14:46:43 +0530

    upload

--- app/components/UserGreeting.tsx
+++ app/components/UserGreeting.tsx
@@ -6,9 +6,6 @@ export default function UserGreeting() {
         <h2 className="text-lg font-semibold">Welcome XYZ</h2>
         <p className="text-sm text-gray-700">PIN: 700034</p>
       </div>
-      <span className="bg-pink-500 text-white px-3 py-1 rounded-full text-sm font-semibold shadow">
-        Siddhanta Ray
-      </span>
     </div>
   );
 }
--- app/uploadSection/page.tsx
+++ app/uploadSection/page.tsx
@@ -1,9 +1,11 @@
 import LocationForm from "@/app/components/LocationForm";
 import FileUploader from "@/app/components/FileUploader";
 import UploadHistory from "@/app/components/UploadHistory";
+import UserGreeting from "@/app/components/userGreeting"
 export default function uploadSection() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#eaffd0] to-[#a7d8a8] mt-16 p-6">
+       <UserGreeting />
       <div className="bg-[#b5dcb7] rounded-lg p-6 mt-4 shadow-md">
         <LocationForm />
         <FileUploader />
