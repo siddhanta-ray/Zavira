834eb48297c03cbe48054451d276cdcc2ebc2e3a
Tameem2004 <mdtameem.gazi2004@gmail.com>
2025-06-22 00:28:31 +0530

    deleted unnecessery files

--- app/uploadSection/page.tsx
+++ app/uploadSection/page.tsx
@@ -1,6 +1,3 @@
-import LocationForm from "@/app/components/LocationForm";
-import FileUploader from "@/app/components/FileUploader";
-import UploadHistory from "@/app/components/UploadHistory";
 import UserGreeting from "@/app/components/UserGreeting"
 import ImageUploadForm from "../components/Image-upload-form";
 export default function uploadSection() {
@@ -8,11 +5,8 @@ export default function uploadSection() {
     <div className="min-h-screen bg-gradient-to-b from-[#eaffd0] to-[#a7d8a8] mt-16 p-6">
       <UserGreeting />
       <div className="bg-[#b5dcb7] rounded-lg p-6 mt-4 shadow-md">
-        {/* <LocationForm /> */}
-        {/* <FileUploader /> */}
         <ImageUploadForm />
       </div>
-      {/* <UploadHistory /> */}
     </div>
   );
 }
